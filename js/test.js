d71685cbacebf5a72deb136a99dc7975beb9191a
ihor-voznyi <haiku.kiev@gmail.com>
2024-06-16 17:28:17 +0300

    add test
    test added

---
+++ js/test.js
@@ -0,0 +1,13 @@
+// Оголоси функцію canRegisterOnSite(age, hasAcceptedTerms),
+// яка перевіряє, чи може користувач зареєструватись на сайті.
+// І повертає true - якщо може зареєструватись і false, якщо - ні.
+// Користувач може зареєструватись, якщо йому виповнилося 13 років
+// і він прийняв умови використання.
+// Значення параметрів будуть задаватися під час виклику функції:
+// age - вік користувача;
+// hasAcceptedTerms - буль (вказує чи прийняв користувач умови використання);
+
+function canRegisterOnSite(age, hasAcceptedTerms) {
+    return age >= 13 && hasAcceptedTerms
+}
+console.log(canRegisterOnSite(12, true));
